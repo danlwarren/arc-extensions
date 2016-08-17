a214af8b3493a5095e6c71a8cfd29cb985e278e7
Dan Warren <dan.l.warren@gmail.com>
2016-08-18 03:18:36 +1000

    More cleanup

--- nncluster.R
+++ nncluster.R
@@ -5,24 +5,23 @@ require(fields)  # Used for Euclidean distance matrices
 nncluster <- function(xy, species){
 
   # Figure out how many species we've got, initialize a data frame
-  species <- as.factor(species)
-  levs <- levels(species)
-  df <- as.data.frame(matrix (nrow=length(levels(species)), ncol=length(levels(species))))
-  rownames(df) <- levels(species)
-  colnames(df) <- levels(species)
+  species.names <- unique(species)
+  df <- as.data.frame(matrix (nrow=length(species.names), ncol=length(species.names)))
+  rownames(df) <- species.names
+  colnames(df) <- species.names
 
   # Do each pair of species
-  for(i in 1:length(levs)){
-    for(j in i:length(levs)){
+  for(i in 1:length(species.names)){
+    for(j in i:length(species.names)){
       if(i != j){
 
         # Need at least two points to calculate
-        if(nrow(xy[species == levs[i],]) > 1 && nrow(xy[species == levs[j],]) > 1){
+        if(nrow(xy[species == species.names[i],]) > 1 && nrow(xy[species == species.names[j],]) > 1){
 
-          print(paste("Calculating", levs[i], "vs", levs[j]))
-          within1 <- rdist(xy[species == levs[i],],xy[species == levs[i],])
-          within2 <- rdist(xy[species == levs[j],],xy[species == levs[j],])
-          between <- rdist(xy[species == levs[i],],xy[species == levs[j],])
+          print(paste("Calculating", species.names[i], "vs", species.names[j]))
+          within1 <- rdist(xy[species == species.names[i],],xy[species == species.names[i],])
+          within2 <- rdist(xy[species == species.names[j],],xy[species == species.names[j],])
+          between <- rdist(xy[species == species.names[i],],xy[species == species.names[j],])
           score1 <- rep(NA,length(within1[1,]))
           score2 <- rep(NA,length(within1[2,]))
 
